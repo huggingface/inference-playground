e7308315ad679252eb66bdb7e8267598ea09fef7
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-05-22 14:36:06 +0100

    fix timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
 	webServer: {
 		command: "npm run build && npm run preview",
+		url: "http://localhost:4173",
 		port: 4173,
+		timeout: 1000 * 60 * 10,
 	},
 	testDir: "e2e",
 });
