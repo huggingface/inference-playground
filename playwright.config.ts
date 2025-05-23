526d485bb498161e73d969a37f14957fd2a3de7a
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-05-23 15:27:06 +0100

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
 	webServer: {
 		command: "npm run build && npm run preview",
-		url: "http://localhost:4173",
 		port: 4173,
 		timeout: 1000 * 60 * 10,
 	},
