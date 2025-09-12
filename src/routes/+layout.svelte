d90a6b06934e943e74eb219697b02eb223502d09
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-09-12 17:40:07 +0100

    import adjustments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,4 @@
 <script lang="ts">
-	import DebugMenu from "$lib/components/debug-menu.svelte";
-	import CustomModelConfig from "$lib/components/inference-playground/custom-model-config.svelte";
-	import ImgPreview from "$lib/components/inference-playground/img-preview.svelte";
-	import Prompts from "$lib/components/prompts.svelte";
-	import QuotaModal from "$lib/components/quota-modal.svelte";
-	import ShareModal from "$lib/components/share-modal.svelte";
 	import IconLoadingLoop from "~icons/line-md/loading-loop";
 	import "../app.css";
 
@@ -22,10 +16,3 @@
 		<IconLoadingLoop class="abs-center absolute text-6xl dark:text-blue-300" />
 	{/snippet}
 </svelte:boundary>
-
-<DebugMenu />
-<Prompts />
-<QuotaModal />
-<ShareModal />
-<CustomModelConfig />
-<ImgPreview />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+	import DebugMenu from "$lib/components/debug-menu.svelte";
+	import CustomModelConfig from "$lib/components/inference-playground/custom-model-config.svelte";
+	import ImgPreview from "$lib/components/inference-playground/img-preview.svelte";
+	import Prompts from "$lib/components/prompts.svelte";
+	import QuotaModal from "$lib/components/quota-modal.svelte";
+	import ShareModal from "$lib/components/share-modal.svelte";
 	import Playground from "$lib/components/inference-playground/playground.svelte";
 	import { conversations } from "$lib/state/conversations.svelte";
 	import { models } from "$lib/state/models.svelte";
@@ -10,3 +16,10 @@
 </script>
 
 <Playground />
+
+<DebugMenu />
+<Prompts />
+<QuotaModal />
+<ShareModal />
+<CustomModelConfig />
+<ImgPreview />
