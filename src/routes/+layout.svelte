2028168580643b58bafeed6b2036f8a2fad6595c
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-09-25 14:09:09 +0100

    update loading icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import IconLoadingLoop from "~icons/line-md/loading-loop";
+	import HuggingFaceLogo from "~icons/logos/hugging-face-icon";
 	import "../app.css";
 
 	interface Props {
@@ -12,7 +13,9 @@
 <svelte:boundary>
 	{@render children?.()}
 	{#snippet pending()}
-		<p class="abs-center absolute dark:text-white">🤗</p>
+		<p class="abs-center absolute dark:text-white">
+			<HuggingFaceLogo class="size-6 text-blue-500" />
+		</p>
 		<IconLoadingLoop class="abs-center absolute text-6xl dark:text-blue-300" />
 	{/snippet}
 </svelte:boundary>
