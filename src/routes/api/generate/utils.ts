be61c3d1f74a07ad6bd5704aae4d2305301b220f
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-11-10 17:33:17 +0000

    improve log

--- src/routes/api/generate/utils.ts
+++ src/routes/api/generate/utils.ts
@@ -2,10 +2,12 @@ const DEBUG_LOG = true;
 
 export const debugLog = (...args: unknown[]) => {
 	if (!DEBUG_LOG) return;
-	console.log("[LOG DEBUG]", ...args);
+	const now = new Date();
+	console.log(`[Time: ${now.toLocaleTimeString()}] [LOG] ${args.join(" ")}`);
 };
 
 export const debugError = (...args: unknown[]) => {
 	if (!DEBUG_LOG) return;
-	console.error("[LOG DEBUG]", ...args);
+	const now = new Date();
+	console.error(`[Time: ${now.toLocaleTimeString()}] [ERROR] ${args.join(" ")}`);
 };
